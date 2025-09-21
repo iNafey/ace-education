822021db6366f96207f7967492aa09b4d8c4109b
iNafey <abdulnafey1@gmail.com>
2025-09-21 15:38:49 +0100

    Google Tag added for Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import Script from "next/script"
 
 const figtree = Figtree({
   subsets: ["latin"],
@@ -24,9 +26,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-36ZW3B5QZE"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-36ZW3B5QZE');
+          `}
+        </Script>
+      </head>
       <body className={`${figtree.className} bg-background text-foreground min-h-screen`}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
